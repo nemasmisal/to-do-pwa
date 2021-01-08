ac4d1e6ad429b117cf44a9d7557701ff16f5cb89
Nikolay <nikolay.topalov@gmail.com>
2021-01-08 14:51:38 +0200

    main, about, contact pages added, basic css, basic js added

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  //nav menu
+  const menu = () => document.querySelector(".side-menu");
+  M.Sidenav.init(menu(), { edge: "right" });
+  //add new task form
+  const form = () =>document.querySelector(".side-form");
+  M.Sidenav.init(form(), { edge: "left" });
+});
